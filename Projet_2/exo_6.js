9d8909dc2e810b7db9961cf0ebdc8f5425d05369
Jade-m22 <mj.micheljade@gmail.com>
2025-05-14 16:18:27 +0200

    exo 6

--- Projet_2/exo_6.js
+++ Projet_2/exo_6.js
@@ -1,18 +1,19 @@
 // 2.3.6. Exercice 6
 // Résous le sujet 2 en ne faisant qu'un seul passage sur ta liste.
 
-function viewBuildingOnce(arr){
-    let maxHigh = 0
-    const view = [] 
-    
-    for(i = arr.length -1 ; i >=0 ; i--){
-        if(arr[i] > maxHigh){
-            view.push(arr[i])
-            maxHigh = arr[i]
-        }
+function viewBuildingOnce(arr) {
+  let maxHigh = 0;
+  let count = 0;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] > maxHigh) {
+      count++;
+      maxHigh = arr[i];
     }
-    return view.length
+  }
+
+  return count;
 }
 
 console.log(viewBuildingOnce([3, 7, 8, 3, 6, 1]));
-console.log(viewBuildingOnceh([1, 4, 5, 8]));
+console.log(viewBuildingOnce([1, 4, 5, 8]));
